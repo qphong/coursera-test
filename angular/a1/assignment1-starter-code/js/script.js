c25496a71c1174a3de46513a1d23126766e947d8
Nguyen Quoc Phong <qphong@live.com>
2016-10-15 04:01:53 +0800

    angularJS week1 solution

---
+++ angular/a1/assignment1-starter-code/js/script.js
@@ -0,0 +1,45 @@
+
+(function(){
+
+	angular.module("LunchApp",[])
+	  .controller("CheckerController", checkerController);
+
+	checkerController.$inject = ['$scope'];
+
+	function checkerController($scope) {
+		$scope.message = "";
+		$scope.dishes = "";
+		$scope.checkLunch = checkLunch;
+		$scope.textColor = "black";
+		$scope.textboxBorderColor = "#ccc";
+
+		function checkLunch(event) {
+			
+			if (!(event instanceof KeyboardEvent) || event.key === "Enter") {
+				console.log('here');
+				if ($scope.dishes === "") {
+					$scope.message = "Please enter data first!";
+					$scope.textColor = "red";
+					$scope.textboxBorderColor = "red";
+					return;
+				}
+
+				dishes = $scope.dishes.split(',');
+
+				var count = 0;
+				for (d in dishes)
+					if (dishes[d].replace(/\s/g, '').length)
+						count++;
+
+				if (count > 3)
+					$scope.message = "Too much!";
+				else
+					$scope.message = "Enjoy!";
+
+				$scope.textColor = "green";
+				$scope.textboxBorderColor = "green";
+			}
+		}
+	}
+
+})();
