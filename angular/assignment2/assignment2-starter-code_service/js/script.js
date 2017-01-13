d10579fd0f8ca836f444da01f51a338ec412e244
Nguyen Quoc Phong <qphong@live.com>
2017-01-13 18:32:20 +0800

    assignment 2

---
+++ angular/assignment2/assignment2-starter-code_service/js/script.js
@@ -0,0 +1,65 @@
+(function(){
+
+	"use strict";
+
+	angular.module("ShoppingListApp",[])
+		.controller("BuyingController", BuyingController)
+		.controller("BoughtController", BoughtController)
+		.service("BuyService", BuyService);
+
+	BuyingController.$inject = ["BuyService"];
+	function BuyingController(BuyService) {
+		var buyingCntrl = this;
+
+		var buyingList = [
+			{name: "Cookies", quantity: 10}
+		];
+
+		for (var i=0; i < buyingList.length; i++) {
+			BuyService.addBuyingItem(buyingList[i]);
+		}
+
+		buyingCntrl.list = BuyService.getBuyingList();
+
+		buyingCntrl.buy = function(idx) {
+			BuyService.buyItemIdx(idx);
+		};
+	}
+
+	BoughtController.$inject = ["BuyService"];
+	function BoughtController(BuyService) {
+		var boughtCntrl = this;
+
+		boughtCntrl.list = BuyService.getBoughtList();
+	}
+
+	function BuyService() {
+
+		var buyService = this;
+		var buyingList = [];
+		var boughtList = [];
+
+		buyService.getBuyingList = function() {
+			return buyingList;
+		};
+
+		buyService.addBuyingItem = function(item) {
+			buyingList.push(item);
+		};
+
+		buyService.getBoughtList = function() {
+			return boughtList;
+		};
+
+		buyService.addBoughtItem = function(item) {
+			boughtList.push(item);
+		};
+
+		buyService.buyItemIdx = function(idx) {
+			var item = buyingList[idx];	
+			buyingList.splice(idx,1);
+			boughtList.push(item); 
+		};
+	}
+
+})();
